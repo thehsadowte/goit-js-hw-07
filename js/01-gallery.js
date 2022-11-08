e4c5b41f4fa8bae7135c7b8d388b5263d92e0c23
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2022-11-09 00:42:13 +0200

    task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,55 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const markup = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+function createGallery(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+    })
+    .join('');
+}
+
+function createModal(image) {
+  const instance = basicLightbox.create(`<img src="${image.dataset.source}">`, {
+    onShow: instance => {
+      document.addEventListener('keydown', closeOnEsc.bind(instance));
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown', closeOnEsc.bind(instance));
+    },
+  });
+
+  instance.show();
+}
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery')) {
+    return;
+  }
+
+  createModal(e.target);
+}
+
+function closeOnEsc(e) {
+  if (e.code === 'Escape') {
+    this.close();
+  }
+}
+
+gallery.addEventListener('click', onImageClick);
